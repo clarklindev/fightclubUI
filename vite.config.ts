409977a8f59daf480af11a3a558ba54df7fb9414
clarklindev <clarklin.dev@gmail.com>
2023-09-15 08:50:29 +0800

    tweaks for library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  base: '/',
-  plugins: [react(), splitVendorChunkPlugin()],
-  build: {
-    target: 'modules', //modules is the default - it support native esmodules / dynamic loading
-    lib: {
-      entry: path.resolve(__dirname, './src/main.tsx'),
-      fileName: 'index', // defaults to package name
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        exports: 'named',
-        sourcemap: true,
+export default ({ command }) => {
+  return defineConfig({
+    base: '/',
+    plugins: [react()],
+    build: {
+      target: 'modules', //modules is the default - it support native esmodules / dynamic loading
+      lib: {
+        entry: path.resolve(__dirname, 'src/main.tsx'),
+        fileName: 'index', // defaults to package name
+        formats: ['es', 'cjs'],
+      },
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          exports: 'named',
+        },
+        // Specify different input file for development
+        input:
+          command === 'serve'
+            ? path.resolve(__dirname, 'src/main.tsx')
+            : undefined,
       },
     },
-  },
-});
+  });
+};
