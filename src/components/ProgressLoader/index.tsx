283b518769413a098039d825e641d96175360238
clarklindev <clarklin.dev@gmail.com>
2023-09-28 12:21:15 +0800

    ProgressLoader Component - refactor - added features color, strokeWidth props - always centered to parent

--- src/components/ProgressLoader/index.tsx
+++ src/components/ProgressLoader/index.tsx
@@ -3,36 +3,47 @@ import styled from 'styled-components';
 
 type ProgressLoaderProps = {
   progress: number; // Progress as a percentage
-  size: number; // Circle size in pixels
+  size?: number; // Circle size in pixels
+  strokeWidth?: number; // Stroke width in pixels
+  color?: string;
 };
 
-export const ProgressLoader: React.FC<ProgressLoaderProps> = ({ progress, size }) => {
-  const radius = size / 2 - size / 10;
+export const ProgressLoader: React.FC<ProgressLoaderProps> = ({
+  progress,
+  size = 20,
+  strokeWidth = 2,
+  color = 'currentColor',
+}) => {
+  const radius = (size - strokeWidth) / 2;
   const circumference = 2 * Math.PI * radius;
   const dashoffset = circumference - (progress / 100) * circumference;
-  const transform = `rotate(-90 ${size / 2} ${size / 2})`;
+  const viewBoxSize = size + strokeWidth * 2; // Adjust viewBox size to accommodate strokeWidth
 
   return (
-    <svg width={size} height={size}>
-      <ProgressLoaderCircle
+    <ProgressLoaderWrapper width={size} height={size} viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`}>
+      <circle
         className="circle"
-        strokeWidth={size / 10}
-        strokeDasharray={`${circumference} ${circumference}`}
+        fill="none"
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeDasharray={circumference}
         strokeDashoffset={dashoffset}
         r={radius}
-        cx={size / 2}
-        cy={size / 2}
-        transform={transform}
+        cx={viewBoxSize / 2}
+        cy={viewBoxSize / 2}
+        transform={`rotate(-90 ${viewBoxSize / 2} ${viewBoxSize / 2})`}
       />
-    </svg>
+    </ProgressLoaderWrapper>
   );
 };
-
 // ------------------------------------------------------------------------------------------------------------------------------------------------
 
-const ProgressLoaderCircle = styled.circle`
-  fill: none;
-  stroke: currentColor;
-  stroke-width: 2;
-  stroke-linecap: round;
+const ProgressLoaderWrapper = styled.svg`
+  //always centered in relation to parent
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
 `;
--- src/pages/ProgressLoader/index.tsx
+++ src/pages/ProgressLoader/index.tsx
@@ -1,14 +1,26 @@
 import { Heading, ProgressLoader } from '../../components';
+import { ResizePanel } from '../../components';
 
 const ProgressLoaderExample = () => {
   return (
     <>
       <Heading variation="h4">Progress Loader</Heading>
       <div className="flex flex-col gap-10">
-        <ProgressLoader progress={25} size={15} />
-        <ProgressLoader progress={50} size={15} />
-        <ProgressLoader progress={75} size={15} />
-        <ProgressLoader progress={100} size={15} />
+        <ResizePanel>
+          <ProgressLoader progress={25} size={20} />
+        </ResizePanel>
+
+        <ResizePanel>
+          <ProgressLoader progress={50} size={20} />
+        </ResizePanel>
+
+        <ResizePanel>
+          <ProgressLoader progress={75} size={20} />
+        </ResizePanel>
+
+        <ResizePanel>
+          <ProgressLoader progress={100} size={20} />
+        </ResizePanel>
       </div>
     </>
   );
