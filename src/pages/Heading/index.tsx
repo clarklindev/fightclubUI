5500917f835288f43b823c9f95f4b04de2950066
clarklindev <clarklin.dev@gmail.com>
2023-11-02 20:11:15 +0800

    Heading example

--- src/pages/Heading/index.tsx
+++ src/pages/Heading/index.tsx
@@ -1,45 +1,50 @@
-import { Heading, ResizePanel, Layout } from '../../components';
+import reactElementToJSXString from 'react-element-to-jsx-string';
+import { Heading, ResizePanel, Layout, CodeBlock, Tabs } from '../../components';
 
 const HeadingExample = () => {
+  const preview = (
+    <>
+      <Heading variation="h1">H1 heading</Heading>
+
+      <Heading variation="h2">H2 heading</Heading>
+
+      <Heading variation="h3">H3 heading</Heading>
+
+      <Heading variation="h4">H4 heading</Heading>
+
+      <Heading variation="h5">H5 heading</Heading>
+
+      <Heading variation="h6">H6 heading</Heading>
+    </>
+  );
+  const previewString = reactElementToJSXString(preview);
+
+  const tabsData = [
+    { label: 'preview', content: preview },
+    {
+      label: 'code',
+      content: (
+        <CodeBlock type="tsx">
+          {`
+import {Heading} from '../../components';
+
+${previewString}
+`}
+        </CodeBlock>
+      ),
+    },
+  ];
+
   return (
     <>
       <Heading variation="h4">Headings</Heading>
 
-      <Layout variation="block">
-        <ResizePanel>
-          <Heading variation="h1">H1 heading</Heading>
-        </ResizePanel>
-      </Layout>
-
-      <Layout variation="block">
-        <ResizePanel>
-          <Heading variation="h2">H2 heading</Heading>
-        </ResizePanel>
-      </Layout>
-
-      <Layout variation="block">
-        <ResizePanel>
-          <Heading variation="h3">H3 heading</Heading>
-        </ResizePanel>
-      </Layout>
-
-      <Layout variation="block">
-        <ResizePanel>
-          <Heading variation="h4">H4 heading</Heading>
-        </ResizePanel>
-      </Layout>
-
-      <Layout variation="block">
-        <ResizePanel>
-          <Heading variation="h5">H5 heading</Heading>
-        </ResizePanel>
-      </Layout>
-
-      <Layout variation="block">
-        <ResizePanel>
-          <Heading variation="h6">H6 heading</Heading>
-        </ResizePanel>
-      </Layout>
+      <Tabs>
+        <Tabs.TabWrapper data={tabsData}>
+          <Tabs.TabHeaders />
+          <Tabs.TabContent />
+        </Tabs.TabWrapper>
+      </Tabs>
     </>
   );
 };
