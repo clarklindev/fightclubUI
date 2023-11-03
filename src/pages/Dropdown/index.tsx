94400dfed2735c83ecff05fcecc203673092ec6f
clarklindev <clarklin.dev@gmail.com>
2023-11-03 15:23:32 +0800

    cleanup

--- src/pages/Dropdown/index.tsx
+++ src/pages/Dropdown/index.tsx
@@ -3,19 +3,17 @@ import reactElementToJSXString from 'react-element-to-jsx-string';
 
 const DropdownExample = () => {
   const preview = (
-    <>
-      <Dropdown>
-        <Dropdown.DropdownWrapper id="2">
-          <Dropdown.DropdownTrigger>hi</Dropdown.DropdownTrigger>
-          <Dropdown.DropdownMenu className="w-40">
-            <Dropdown.DropdownMenuItem>A</Dropdown.DropdownMenuItem>
-            <Dropdown.DropdownMenuItem>B</Dropdown.DropdownMenuItem>
-            <Dropdown.DropdownMenuItem>C</Dropdown.DropdownMenuItem>
-            <Dropdown.DropdownMenuItem>D</Dropdown.DropdownMenuItem>
-          </Dropdown.DropdownMenu>
-        </Dropdown.DropdownWrapper>
-      </Dropdown>
-    </>
+    <Dropdown>
+      <Dropdown.DropdownWrapper id="2">
+        <Dropdown.DropdownTrigger>hi</Dropdown.DropdownTrigger>
+        <Dropdown.DropdownMenu className="w-40">
+          <Dropdown.DropdownMenuItem>A</Dropdown.DropdownMenuItem>
+          <Dropdown.DropdownMenuItem>B</Dropdown.DropdownMenuItem>
+          <Dropdown.DropdownMenuItem>C</Dropdown.DropdownMenuItem>
+          <Dropdown.DropdownMenuItem>D</Dropdown.DropdownMenuItem>
+        </Dropdown.DropdownMenu>
+      </Dropdown.DropdownWrapper>
+    </Dropdown>
   );
 
   const previewString = reactElementToJSXString(preview);
