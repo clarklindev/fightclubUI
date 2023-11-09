4b5e4a81e9247b54576cc96a531815550065486a
clarklindev <clarklin.dev@gmail.com>
2023-11-09 18:53:02 +0800

    preview pages

--- src/pages/Icon/index.tsx
+++ src/pages/Icon/index.tsx
@@ -1,15 +1,45 @@
-import { Icon, Heading, ResizePanel } from '@swagfinger/components';
+import reactElementToJSXString from 'react-element-to-jsx-string';
+
+import { Icon, Heading, Tabs, CodeBlock } from '@swagfinger/components';
 import { ShoppingCartIcon } from '@swagfinger/icons';
 
 const IconExample = () => {
+  const preview = (
+    <>
+      <Icon size="20px">
+        <ShoppingCartIcon />
+      </Icon>
+    </>
+  );
+
+  const previewString = reactElementToJSXString(preview);
+
+  const tabsData = [
+    { label: 'preview', content: preview },
+    {
+      label: 'code',
+      content: (
+        <CodeBlock type="tsx">
+          {`
+import {Icon} from '@swagfinger/components';
+
+${previewString}
+`}
+        </CodeBlock>
+      ),
+    },
+  ];
+
   return (
     <>
       <Heading variation="h4">Icon</Heading>
-      <ResizePanel>
-        <Icon size="20px">
-          <ShoppingCartIcon />
-        </Icon>
-      </ResizePanel>
+
+      <Tabs>
+        <Tabs.TabWrapper data={tabsData}>
+          <Tabs.TabHeaders />
+          <Tabs.TabContent />
+        </Tabs.TabWrapper>
+      </Tabs>
     </>
   );
 };
--- src/pages/Input/index.tsx
+++ src/pages/Input/index.tsx
@@ -1,4 +1,7 @@
-import { Layout, Heading, ResizePanel } from '@swagfinger/components';
+import reactElementToJSXString from 'react-element-to-jsx-string';
+
+import { useState, useEffect } from 'react';
+import { Layout, Heading, Tabs, CodeBlock } from '@swagfinger/components';
 
 import InputBasicExample from './InputBasicExample';
 import InputReadOnlyExample from './InputReadOnlyExample';
@@ -9,57 +12,83 @@ import InputFilterExample from './InputFilterExample';
 import InputSearchExample from './InputSearchExample';
 
 const InputExample = () => {
+  const [fileContent, setFileContent] = useState('');
+
+  useEffect(() => {
+    // Define the relative path to your file within the public directory
+
+    const fetchData = async () => {
+      const relativeFilePath = './src/pages/Input/InputNoBorderExample.tsx'; // Adjust the file extension and path
+      try {
+        const response = await fetch(relativeFilePath);
+        if (response.ok) {
+          const data = await response.text();
+          console.log('DATA: ', data);
+          setFileContent(data);
+        } else {
+          console.error(`Failed to fetch file: ${response.statusText}`);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const preview_noborder = (
+    <Layout variation="block">
+      <Heading variation="h5">Input (no-border)</Heading>
+      <InputNoBorderExample />
+    </Layout>
+  );
+
+  const tabsData = [
+    { label: 'preview', content: preview_noborder },
+    {
+      label: 'code',
+      content: '',
+    },
+  ];
+
   return (
     <>
       <Heading variation="h4">Input</Heading>
 
-      <Layout variation="block">
-        <Heading variation="h5">Input (no-border)</Heading>
-        <ResizePanel>
-          <InputNoBorderExample />
-        </ResizePanel>
-      </Layout>
+      <Tabs>
+        <Tabs.TabWrapper data={tabsData}>
+          <Tabs.TabHeaders />
+          <Tabs.TabContent />
+        </Tabs.TabWrapper>
+      </Tabs>
 
       <Layout variation="block">
         <Heading variation="h5">Basic input</Heading>
-        <ResizePanel>
-          <InputBasicExample />
-        </ResizePanel>
+        <InputBasicExample />
       </Layout>
 
       <Layout variation="block">
         <Heading variation="h5">Input (readonly)</Heading>
-        <ResizePanel>
-          <InputReadOnlyExample />
-        </ResizePanel>
+        <InputReadOnlyExample />
       </Layout>
 
       <Layout variation="block">
         <Heading variation="h5">Input + Icon</Heading>
-        <ResizePanel>
-          <InputWithIconExample />
-        </ResizePanel>
+        <InputWithIconExample />
       </Layout>
 
       <Layout variation="block">
         <Heading variation="h5">Input Password</Heading>
-        <ResizePanel>
-          <InputPasswordExample />
-        </ResizePanel>
+        <InputPasswordExample />
       </Layout>
 
       <Layout variation="block">
         <Heading variation="h5">Input Filter</Heading>
-        <ResizePanel>
-          <InputFilterExample />
-        </ResizePanel>
+        <InputFilterExample />
       </Layout>
 
       <Layout variation="block">
         <Heading variation="h5">Input Search</Heading>
-        <ResizePanel>
-          <InputSearchExample />
-        </ResizePanel>
+        <InputSearchExample />
       </Layout>
     </>
   );
