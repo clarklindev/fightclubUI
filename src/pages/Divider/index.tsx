bd175f40e057cbf3d558acc38479839ba3cfccbe
clarklindev <clarklin.dev@gmail.com>
2023-11-18 20:45:17 +0800

    CodeBlock.Playground - for interactive

--- src/pages/Divider/index.tsx
+++ src/pages/Divider/index.tsx
@@ -61,9 +61,9 @@ const DividerExample = () => {
               <CodeBlock.Preview />
             </Tabs.Content>
             <Tabs.Content data-tab="1">
-              <CodeBlock editable={true} readOnly={false}>
+              <CodeBlock.Playground editable={true} readOnly={false}>
                 {preview}
-              </CodeBlock>
+              </CodeBlock.Playground>
             </Tabs.Content>
           </Tabs.ContentGroup>
         </Tabs>
