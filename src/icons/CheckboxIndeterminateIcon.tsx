68cba4e39efeeb7c7b0ad6bef0c7b3e39ef55786
clarklindev <clarklin.dev@gmail.com>
2023-11-25 14:39:04 +0800

    remove icon from lib and use 3rd party library icons

--- src/icons/CheckboxIndeterminateIcon.tsx
+++
@@ -1,7 +0,0 @@
-export const CheckboxIndeterminateIcon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor">
-      <path strokeLinecap="square" d="M18 12H6" />
-    </svg>
-  );
-};
