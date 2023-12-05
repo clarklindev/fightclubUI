cb63a83aad533b4949db5e37338a1a2ebb9bffd9
clarklindev <clarklin.dev@gmail.com>
2023-12-05 09:00:54 +0800

    adjust tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: ['class', '[data-color-scheme="dark"]'],
+  // usually dont need safelist, but we include it because we want to use dynamic values
   safelist: [
     {
-      pattern: /^(gap-|space-|p-|m-|w-|h-|border-|before:h-|before:w-)/,
+      pattern: /^(gap-|space-|p-|m-|w-|h-|border-)/,
+      variants:['before','after']
     },
   ],
   theme: {
