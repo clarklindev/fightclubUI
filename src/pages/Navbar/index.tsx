5038045b6fd1e8ad1582c131347a84af7d90b9ee
clarklindev <clarklin.dev@gmail.com>
2023-12-14 10:08:02 +0800

    Navbar, NavSide, OnThisPage documentation

--- src/pages/Navbar/index.tsx
+++ src/pages/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading } from '@swagfinger/components';
+import { Card, Heading, Layout } from '@swagfinger/components';
 
 const Navbar = () => {
   return (
@@ -7,6 +7,22 @@ const Navbar = () => {
       <Heading variation="h1" data-observable="true">
         Navbar
       </Heading>
+
+      <Layout className="mt-5">
+        <Card>
+          <nav className="bg-blue-500 p-4 text-white">Navbar</nav>
+
+          <div className="container mx-auto mt-4">
+            <div className="grid grid-cols-8 grid-rows-2 gap-4">
+              <div className="bg-gray-600 h-32 p-4 col-span-2 row-span-2">NavSide</div>
+
+              <div className="bg-gray-600 h-32 p-4 col-span-4 row-span-2">Main content area</div>
+
+              <div className="bg-gray-600 h-32 p-4 col-span-2 row-span-2">OnThisPage</div>
+            </div>
+          </div>
+        </Card>
+      </Layout>
     </>
   );
 };
--- src/pages/OnThisPage/index.tsx
+++ src/pages/OnThisPage/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading } from '@swagfinger/components';
+import { Heading, Card, Layout } from '@swagfinger/components';
 
 const OnThisPage = () => {
   return (
@@ -7,6 +7,22 @@ const OnThisPage = () => {
       <Heading variation="h1" data-observable="true">
         OnThisPage
       </Heading>
+
+      <Layout className="mt-5">
+        <Card>
+          <nav className="bg-gray-600 p-4">Navbar</nav>
+
+          <div className="container mx-auto mt-4">
+            <div className="grid grid-cols-8 grid-rows-2 gap-4">
+              <div className="bg-gray-600 h-32 p-4 col-span-2 row-span-2">NavSide</div>
+
+              <div className="bg-gray-600 h-32 p-4 col-span-4 row-span-2">Main content area</div>
+
+              <div className="bg-blue-500 text-white h-32 p-4 col-span-2 row-span-2">OnThisPage</div>
+            </div>
+          </div>
+        </Card>
+      </Layout>
     </>
   );
 };
