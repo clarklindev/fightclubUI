220292c5e9d2394f88caaceb975a67ee778efde8
clarklindev <clarklindev@gmail.com>
2025-03-24 21:27:58 +0800

    Layouts

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const Badge = ({ className, children }: { className?: string; children: React.ReactNode }) => {
   return (
--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,4 +1,5 @@
 import { Heading, Layout, Text } from '@fightclub/components';
+import { CodeInline } from '@fightclub/components/CodeInline';
 
 const Layouts = () => {
   return (
@@ -8,13 +9,26 @@ const Layouts = () => {
       </Heading>
 
       <Layout>
-        <Text>The Layout component is specifically used to apply layout styles, this is different from Layouts.</Text>
+        <Text>The Layout component is specifically used to apply layout styles, this is different from a page's Layouts.</Text>
+        <Text>
+          Here is an example of how to use the Layout Component; You usually wrap other components with Layout. By default there is a tailwind class applied of `pb-8` (padding bottom)
+          <br/>
+          <CodeInline value={
+`<Layout>
+  <Heading variation="h5" size="level2">
+    Icon button
+  </Heading>
+</Layout>
+`
+          }/>
+        </Text>
         <br />
         <Text>
-          Layouts is the template that defines the way items are placed on the page. By creating different layouts, they
+          Layouts on the otherhand is the template that defines the way items are placed on the page. By creating different layouts, they
           allow you to present data differently. Layouts should be interchangeable for pages with ability to switch
           layouts.
         </Text>
+
       </Layout>
     </div>
   );
