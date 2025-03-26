9f061dac277b790b1a1c1de8fa714b58c84048e9
clarklindev <clarklindev@gmail.com>
2025-03-27 01:43:35 +0800

    revert SliderMultiRange

--- src/components/SliderMultiRange/index.tsx
+++ src/components/SliderMultiRange/index.tsx
@@ -1,7 +1,16 @@
-import React, { useState, useRef, memo, useEffect } from 'react';
+import React, { useState, useRef, forwardRef, useEffect, useLayoutEffect } from 'react';
 import { Slider } from '@fightclub/components';
 import { Orientation } from '@fightclub/types/Orientation';
-import { debounce } from 'lodash';
+
+//valueGradient
+// const startPercentage = sliderValue; // Adjust this value to set the start point
+// const endPercentage = '80%'; // Adjust this value to set the start point
+//middle
+// start would be previous' end
+// end would be next's start
+//const valueGradient =  `linear-gradient(90deg, transparent 0%, transparent ${startPercentage}%, ${activeColor} ${sliderValue}%,  ${activeColor} ${endPercentage}, transparent ${endPercentage} )`;
+//last
+//const valueGradient =  `linear-gradient(90deg, transparent 0%, transparent ${sliderValue}%, ${activeColor} ${sliderValue}%,  ${activeColor} ${endPercentage}, transparent ${endPercentage} )`;
 
 enum SlideMode {
   RESTRICT = 'restrict',
@@ -24,10 +33,9 @@ type SliderMultiRangeProps = {
   hideTrack?: boolean;
 };
 
-const SliderMemo = memo(Slider);
-
 export const SliderMultiRange = ({
   sliderValues = [0, 0, 0],
+  // colors = ['red', 'yellow', 'blue'],
   onChange,
   min = 0,
   max = 100,
@@ -41,8 +49,9 @@ export const SliderMultiRange = ({
   const [activeIndex, setActiveIndex] = useState<number | null>(null); // To track the active element
 
   const restrictBoundaries = (index: number, value: number) => {
-    let _min: number = sliderValues.length === 1 || index === 0 ? min : sliderValues[index - 1];
-    let _max: number = sliderValues.length === 1 || index === sliderValues.length - 1 ? max : sliderValues[index + 1];
+    //check if single element in sliderValues || if last element in sliderValues
+    let _min: number = sliderValues.length === 1 || index === 0 ? min : sliderValues[index - 1]; //can check index-1 because index (0) is covered
+    let _max: number = sliderValues.length === 1 || index === sliderValues.length - 1 ? max : sliderValues[index + 1]; //can check index+1 because index (sliderValues.length - 1) is covered
 
     if (value <= _min) {
       value = _min;
@@ -53,27 +62,38 @@ export const SliderMultiRange = ({
     return value;
   };
 
-  // Debounced handler to optimize the onChange event
-  const debouncedOnChangeHandler = debounce((value: number, index: number) => {
-    let updatedValues: Array<number> = [...sliderValues];
+  //function that gets called everytime one of the sliders value changes
+  const onChangeHandler = (value: number, index = 0) => {
+    let updatedValues;
 
     setActiveIndex(index);
 
     if (slideMode === SlideMode.RESTRICT) {
-      updatedValues[index] = restrictBoundaries(index, value);
+      //restrict method - values dont go below prev or over next's value
+      const restricted = restrictBoundaries(index, value);
+      updatedValues = [...sliderValues];
+      updatedValues[index] = restricted;
     }
 
     if (slideMode === SlideMode.MAGNETIC) {
-      updatedValues[index] = value;
+      //magnet effect
+      // change other values too - slide other values
+      updatedValues = [...sliderValues];
+      // //if its opposite direction to linked movement
+      // //note if you use a value of <=1 and >= 1 in the difference if() check, it will have a magnetic effect. we use 0.5
       if (value < updatedValues[index]) {
-        for (let i = index; i > 0; i--) {
+        updatedValues[index] = value;
+        for (var i = index; i > 0; i--) {
+          //use comparison <=1 for magnetic effect
           if (updatedValues[i] - updatedValues[i - 1] <= 0) {
             updatedValues[i - 1] = updatedValues[i];
           }
         }
       }
       if (value > updatedValues[index]) {
-        for (let i = index; i < sliderValues.length - 1; i++) {
+        updatedValues[index] = value;
+        for (var i = index; i < sliderValues.length - 1; i++) {
+          //use comparison <=1 for magnetic effect
           if (updatedValues[i + 1] - updatedValues[i] <= 0) {
             updatedValues[i + 1] = updatedValues[i];
           }
@@ -82,14 +102,15 @@ export const SliderMultiRange = ({
     }
 
     if (slideMode === SlideMode.SLIDETHROUGH) {
+      //mode is slidethrough
+      updatedValues = [...sliderValues];
       updatedValues[index] = value;
     }
 
-    // Only update the parent component if the value has changed
-    if (updatedValues !== sliderValues) {
+    if (updatedValues) {
       onChange(updatedValues);
     }
-  }, 100); // Adjust debounce delay as needed
+  };
 
   const sliderMultiRangeRef = useRef<HTMLDivElement>(null);
 
@@ -101,14 +122,13 @@ export const SliderMultiRange = ({
         orientation === Orientation.HORIZONTAL && 'w-full',
         orientation === Orientation.VERTICAL && 'h-full',
         'relative',
-      ].join(' ')}
-    >
+      ].join(' ')}>
       <div
         data-component="SliderTrack"
-        className="absolute border rounded-full bg-orange-500"
+        className={[`absolute border rounded-full bg-orange-500`].join(' ')}
         style={{
           ...(orientation === Orientation.HORIZONTAL && {
-            width: '100%',
+            width: `100%`,
             height: `${thickness}px`,
           }),
           ...(orientation === Orientation.VERTICAL && {
@@ -117,32 +137,31 @@ export const SliderMultiRange = ({
           }),
         }}
       />
-      
+
       {(sliderValues || []).map((sliderValue, index) => {
+        //this controls how the items are placed
         const calculatedOffset =
           orientation === Orientation.HORIZONTAL
-            ? `${thumbSize * index}px`
+            ? thumbSize * index + 'px'
             : `${(sliderValues.length - 1 - index) * thumbSize}px`;
-
         return (
-          <SliderMemo
+          <Slider
             orientation={orientation}
             key={index}
             value={sliderValue}
             index={index}
-            className="absolute"
-            onChange={(value: number) => debouncedOnChangeHandler(value, index)}
+            className={'absolute'} /* this stacks the scrollbars in horizontal mode*/
+            onChange={onChangeHandler}
             length={
-              slideMode === SlideMode.SLIDETHROUGH
-                ? '100%'
-                : `calc(100% - ${(sliderValues.length - 1) * thumbSize}px)`
+              slideMode === SlideMode.SLIDETHROUGH ? '100%' : `calc(100% - ${(sliderValues.length - 1) * thumbSize}px)`
             }
             min={min}
             max={max}
-            style={{ zIndex: index === activeIndex ? 1 : 0 }}
+            style={{ zIndex: index === activeIndex ? 1 : 0 }} //z-index
             offset={slideMode === SlideMode.SLIDETHROUGH ? '0px' : calculatedOffset}
-            trackClickable={trackClickable}
-            hideTrack={hideTrack}
+            //x position to place the <Slider/> you cant see this of each individual slider if position="absolute". only when className = "" and hideTrack="false"
+            trackClickable={trackClickable} //you want to leave this FALSE for multirange input
+            hideTrack={hideTrack} //you want to leave this as TRUE for multirange input - <SliderTrack /> replaces this
             thumbSize={thumbSize}
             thickness={thickness}
           />
