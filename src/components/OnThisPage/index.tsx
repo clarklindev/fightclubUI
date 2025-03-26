8a3c98ffc84c1a764da56ac0d9809d2089ef1878
clarklindev <clarklindev@gmail.com>
2025-03-27 05:43:36 +0800

    OnThisPage component

--- src/components/OnThisPage/index.tsx
+++ src/components/OnThisPage/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef, forwardRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-
 import { Button } from '@fightclub/components';
 import { useOnThisPage } from '@fightclub/context/OnThisPageContext';
 import { useScroll } from '@fightclub/context/ScrollContext';
@@ -17,50 +16,51 @@ export const OnThisPage = ({ ...props }) => {
 const Container = ({ ...props }) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const observerRef = useRef<IntersectionObserver | null>(null);
-
   const location = useLocation();
   const currentPage = location.pathname;
 
   const { observablesInView, setObservablesInView, observables, setObservables } = useOnThisPage();
   const { scrollPercentage, scrollToPercentage } = useScroll();
   const selectedItemIndex = useRef<number | null>(null);
+  const lastScrollY = useRef<number>(0);  // Track scroll direction
+
+  // Detecting the scroll direction
+  const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('down');
+  
+  useEffect(() => {
+    const handleScrollDirection = () => {
+      const currentScrollY = window.scrollY;
+      setScrollDirection(currentScrollY > lastScrollY.current ? 'down' : 'up');
+      lastScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener('scroll', handleScrollDirection);
+    return () => window.removeEventListener('scroll', handleScrollDirection);
+  }, []);
 
   useEffect(() => {
-    console.log('\n\n\n\n\n');
-    console.log('CURRENT PAGE CHANGE');
     let observableDom: Array<HTMLElement>;
     const main = document.querySelector('main');
-
+    
     if (main) {
-      console.log('BEGIN----------------------------');
       observableDom = Array.from(main.querySelectorAll('[data-observable="true"]'));
-      console.log('SET OBSERVABLES');
       setObservables(observableDom);
       setObservablesInView(new Array(observableDom.length).fill(false));
-
-      console.log('END----------------------------');
     }
   }, [currentPage]);
 
   const intersectionCallback = async (entries: IntersectionObserverEntry[]): Promise<void> => {
-    console.log('CALLBACK-------------------------------');
-
     if (observables) {
-      // Create a new array with all elements set to false initially
       const newObservablesInView = new Array(observables.length).fill(false);
-      // Check if the selectedItemIndex is set, and set it to true in the new array
-      if (selectedItemIndex.current !== null) {
-        newObservablesInView[selectedItemIndex.current] = true;
-        // Reset selectedItemIndex to null
-        selectedItemIndex.current = null;
-      } else {
-        // Find the entry closest to the top within the viewport
+
+      // Check for the active element based on scroll direction
+      let activeIndex: number | null = null;
+
+      if (scrollDirection === 'down') {
+        // Scroll Down: Prioritize the first intersecting observable
         const firstIntersectingEntry = entries.reduce(
           (closestEntry: IntersectionObserverEntry | null, entry: IntersectionObserverEntry) => {
-            if (
-              entry.isIntersecting &&
-              (!closestEntry || entry.boundingClientRect.top < closestEntry.boundingClientRect.top)
-            ) {
+            if (entry.isIntersecting && (!closestEntry || entry.boundingClientRect.top < closestEntry.boundingClientRect.top)) {
               return entry;
             }
             return closestEntry;
@@ -69,19 +69,42 @@ const Container = ({ ...props }) => {
         );
 
         if (firstIntersectingEntry) {
-          console.log('firstIntersectingEntry: ', (firstIntersectingEntry?.target as HTMLElement).textContent);
-          // Try to find the entry's index in observables
-          const index = observables.indexOf(firstIntersectingEntry.target as HTMLElement);
+          activeIndex = observables.indexOf(firstIntersectingEntry.target as HTMLElement);
+        }
+      } else {
+        // Scroll Up: Prioritize the last intersecting observable
+        const lastIntersectingEntry = entries.reduce(
+          (closestEntry: IntersectionObserverEntry | null, entry: IntersectionObserverEntry) => {
+            if (entry.isIntersecting && (!closestEntry || entry.boundingClientRect.bottom > closestEntry.boundingClientRect.bottom)) {
+              return entry;
+            }
+            return closestEntry;
+          },
+          null,
+        );
 
-          if (index !== -1) {
-            // Update the value in the new array with the most recent isIntersecting value
-            newObservablesInView[index] = true;
-          }
+        if (lastIntersectingEntry) {
+          activeIndex = observables.indexOf(lastIntersectingEntry.target as HTMLElement);
         }
       }
 
+      // Update observablesInView if a valid activeIndex is found
+      if (activeIndex !== null) {
+        newObservablesInView[activeIndex] = true;
+      }
+
+      // If any observable has moved out of view (bottom-up scroll), reset its active state
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          const index = observables.indexOf(entry.target as HTMLElement);
+          if (index !== -1 && newObservablesInView[index] === true) {
+            newObservablesInView[index] = false;
+          }
+        }
+      });
+
+      // Set the new state if the observablesInView array has changed
       if (newObservablesInView.some(value => value === true)) {
-        // Set the updated array as the new state
         setObservablesInView(newObservablesInView);
       }
     }
@@ -89,38 +112,25 @@ const Container = ({ ...props }) => {
 
   useEffect(() => {
     let observer: IntersectionObserver;
-    console.log('CREATE OBSERVER');
     observer = new IntersectionObserver(intersectionCallback, {
       rootMargin: '0px 0px 0px 0px',
+      threshold: 0.5, // Trigger when 50% of an element is in view
     });
-    //IntersectionObserverCallback function()
 
-    console.log('NEW OBSERVE');
     if (observables) {
       observables.forEach(observable => {
         observer.observe(observable);
       });
 
-      console.log('ASSIGN OBSERVER REF');
       observerRef.current = observer;
 
       return () => {
-        console.log('CLEANUP START-------------------------');
         if (observerRef.current) {
-          console.log('UNOBSERVE');
           observables.forEach(observable => {
-            if (observerRef.current) {
-              observerRef.current.unobserve(observable); // Use the observer from the ref
-            }
+            observerRef.current.unobserve(observable);
           });
-          console.log('DISCONNECT');
           observerRef.current.disconnect();
         }
-
-        if (selectedItemIndex.current) {
-          selectedItemIndex.current = null;
-        }
-        console.log('CLEANUP END-------------------------');
       };
     }
   }, [observables]);
@@ -132,18 +142,17 @@ const Container = ({ ...props }) => {
   const [calculatedHeight, setCalculatedHeight] = useState<undefined | string>();
 
   useEffect(() => {
-    setCalculatedHeight(`calc(100vh - 50px - 8rem)`); //minus navbar height, //minus padding * 2
+    setCalculatedHeight(`calc(100vh - 50px - 8rem)`); // subtract navbar height and padding
   }, []);
 
   return (
-    <div className={[`hidden`, `overflow-y-hidden`, ` border-l border-[var(--border-color)]`].join(' ')} {...props}>
+    <div className="hidden overflow-y-hidden border-l border-[var(--border-color)]" {...props}>
       <div
         data-component="Container"
         ref={containerRef}
-        className={[
-          `p-4 radius-full overflow-hidden border border-[var(--border-color)] overscroll-contain hover:overflow-y-hidden`,
-        ].join(' ')}
-        style={{ height: calculatedHeight }}>
+        className="p-4 radius-full overflow-hidden border border-[var(--border-color)] overscroll-contain hover:overflow-y-hidden"
+        style={{ height: calculatedHeight }}
+      >
         {observables && observables.length > 0 ? (
           <div className="flex flex-col relative">
             {observables.map((observable, index) => (
@@ -151,23 +160,11 @@ const Container = ({ ...props }) => {
                 intent="plain"
                 padding="none"
                 key={index}
-                className={[
-                  'truncate',
-                  'flex',
-                  'content-start',
-                  `${
-                    observablesInView && observablesInView[index]
-                      ? [
-                          'before:absolute',
-                          'before:w-[2px]',
-                          'before:h-6',
-                          'before:bg-red-500',
-                          'before:ml-[-16px]',
-                          "before:content-''",
-                        ].join(' ')
-                      : ''
-                  }`,
-                ].join(' ')}
+                className={`truncate flex content-start ${
+                  observablesInView && observablesInView[index]
+                    ? 'before:absolute before:w-[2px] before:h-6 before:bg-red-500 before:ml-[-16px] before:content-""'
+                    : ''
+                }`}
                 onClick={e => {
                   e.preventDefault();
                   selectedItemIndex.current = index;
@@ -175,7 +172,8 @@ const Container = ({ ...props }) => {
                   const targetPosition =
                     index === 0 ? 0 : observable.getBoundingClientRect().top + window.scrollY + offset;
                   window.scrollTo({ top: targetPosition, behavior: 'instant' });
-                }}>
+                }}
+              >
                 {observable.textContent}
               </Button>
             ))}
